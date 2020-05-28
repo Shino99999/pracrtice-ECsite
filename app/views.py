f6a32107ac537884cc381d3b44389e19ef14d3d2
shi <sssoft999@outlook.jp>
2020-05-28 22:27:49 +0900

    initial commit

---
+++ app/views.py
@@ -0,0 +1,22 @@
+# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import (
+    LoginView, LogoutView
+)
+from django.views import generic
+from .forms import LoginForm
+
+
+class Top(generic.TemplateView):
+    template_name = 'register/top.html'
+
+
+class Login(LoginView):
+    """ログインページ"""
+    form_class = LoginForm
+    template_name = 'register/login.html'
+
+
+class Logout(LogoutView):
+    """ログアウトページ"""
+    template_name = 'register/top.html'
